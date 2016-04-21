3b44f7b6a8171db68e5d943da01e4c9d9eecc144
Jazz Lyles <ripley36706@gmail.com>
2016-04-20 20:31:09 -0700

    fixed bio content

--- js/controllers/aboutCtrl.js
+++ js/controllers/aboutCtrl.js
@@ -2,6 +2,6 @@ angular
     .module("app")
     .controller("aboutCtrl", ["$scope", function($scope){
         $scope.title = "About Jazz";
-        $scope.bio = "I began my unconventional journey into the world of software engineering after a full life of adventure. Photography and creativity underlined every point of action. Celebrating the company of Cirque du Soleil performers and world-renowned photographer, Peter Lik, I have always been filled with vibrant exploits into the unknown and reinvented the world through creative solutions. Inspired to extend that principle, I set off to teach myself a new set of skills that has helped me actualize my passion for ideation into visual platforms that touch and engage a multitude of people. Learning Javascript, HTML/CSS, D3 and other programming concepts has helped me adapt my inner dreamer into a materialized reality. I look to the future to usher in a new era of development both personally and professionally.";
+        $scope.bio = "I'm a software engineer with a game available on the App/Play Stores looking to take my existing skills in game development, frontend frameworks, databases, digital asset management and servers and contribute to a team where each engineer is dedicated to best practices and maintaining a fun working environment. I am tech-savvy, goal-driven and have a talent for new technology, problem-solving, interpersonal relations, negotiation and time-management.";
         $scope.quote = "“It always seems impossible until it’s done.” -Nelson Mandela";
     }]);
